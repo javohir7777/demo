a755f8c5fbd61cc5a9a4eae1a62ec1c57fc41258
javohir7777 <javohirjumayev7777@gmail.com>
2024-03-12 01:22:48 -0700

    hello

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+// import { NavLink } from "react-router-dom";
+import bgImd from "./assets/Frame.svg";
+import "./Card.scss";
+function App() {
+  return (
+    <div className="bgRow">
+      <div className="bgCols">
+        <div className="bgImgEdu">
+          <img src={bgImd} alt="" />
+        </div>
+        <div className="bgTextsEdu">
+          <p className="bg-text">Ta’lim</p>
+          <p className="bg-title">
+            Zamonaviy texnologiya va metodikalar asosida IT yo‘nalishlarini
+            o‘qitish
+          </p>
+        </div>
+        <a to="/" className="bg-btn">
+          Batafsil
+        </a>
+      </div>
+      <div className="bgCols">
+        <div className="bgImgEdu">
+          <img src={bgImd} alt="" />
+        </div>
+        <div className="bgTextsEdu">
+          <p className="bg-text">Ta’lim</p>
+          <p className="bg-title">
+            Zamonaviy texnologiya va metodikalar asosida IT yo‘nalishlarini
+            o‘qitish
+          </p>
+        </div>
+        <a to="/" className="bg-btn">
+          Batafsil
+        </a>
+      </div>
+      <div className="bgCols">
+        <div className="bgImgEdu">
+          <img src={bgImd} alt="" />
+        </div>
+        <div className="bgTextsEdu">
+          <p className="bg-text">Ta’lim</p>
+          <p className="bg-title">
+            Zamonaviy texnologiya va metodikalar asosida IT yo‘nalishlarini
+            o‘qitish
+          </p>
+        </div>
+        <a to="/" className="bg-btn">
+          Batafsil
+        </a>
+      </div>
+      <div className="bgCols">
+        <div className="bgImgEdu">
+          <img src={bgImd} alt="" />
+        </div>
+        <div className="bgTextsEdu">
+          <p className="bg-text">Ta’lim</p>
+          <p className="bg-title">
+            Zamonaviy texnologiya va metodikalar asosida IT yo‘nalishlarini
+            o‘qitish
+          </p>
+        </div>
+        <a to="/" className="bg-btn">
+          Batafsil
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
